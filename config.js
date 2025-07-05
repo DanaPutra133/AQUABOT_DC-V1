72378b0a917d89c0a2abe7e6aeb296a1449a2c9f
dana <danaputra1001@gmail.com>
2025-07-05 15:16:29 +0700

    V1

---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // Bot Identity
+  botName: "BetaBotz", // Nama bot kamu
+  ownerID: "BetaBotz", // Ganti dengan Nama Kamu
+
+  // Discord Settings
+  token:
+    "token kamu", // Ganti dengan token bot kamu
+  prefix: "!", // Prefix utama untuk bot ini
+  guildID: "345j634563456", // ID server tempat bot ini berada
+
+  // API Keys
+  apikey_lann: "wh45r3456", // Isi APIkey dari https://api.betabotz.eu.org
+
+  // Menu & Feature Settings
+  menuPublic: false, // Apakah menu public aktif?
+
+  // Channel IDs
+  joinChannelId: "3456j345h6g34h5", // ID channel join
+  gempaChannelId: "3h45634", // ID channel gempa
+  growgardenChannelId: "345h63456", // ID channel growgarden
+  channelIds: {
+    rules: "34h56345", // ID channel rules
+    pricelist: "3h4563456", // ID channel pricelist
+    ticket: "3h4563456", // ID channel ticket
+    queue: "435h63456", // ID channel queue
+  },
+};

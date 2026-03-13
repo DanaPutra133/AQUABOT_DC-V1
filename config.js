1cedda081cc92dba2ea4779022a9ce5fd164f2af
danaputra133 <danaputra375@gmail.com>
2026-03-13 15:17:39 +0700

    fix fitur play udpate DJS/v

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-require("dotenv").config(); // Tambahkan baris ini di paling atas file config.js
+require('dotenv').config(); // Tambahkan baris ini di paling atas file config.js
 
 module.exports = {
   // Bot Identity
@@ -7,41 +7,47 @@ module.exports = {
 
   //API DATABASE RPG
   api: {
-    baseUrl: "https://api.danafxc.my.id/api/proxy", // kamu bisa dapat apikey ini dengan beli di https://api.danafxc.my.id
-    apiKey: process.env.API_KEY_DANA,
-  },
+        baseUrl: "https://api.danafxc.my.id/api/proxy",   // kamu bisa dapat apikey ini dengan beli di https://api.danafxc.my.id
+        apiKey: process.env.API_KEY_DANA, 
+    },
 
   // Discord Settings
   token: process.env.DISCORD_BOT_TOKEN, // Mengambil token dari .env
   prefix: "!", // Prefix utama untuk bot ini
 
-  clientId: "", //client ID bot kamu
-
+  clientId:  "", //client ID bot kamu
+  
   //ini pakai kalau misal nya slash / command mau di 1 server aja, optional
   guildID: "", // ID server tempat bot ini berada
 
   //fitur braodcast yang bakal di kirim ke beberapa ch yang di input
-  broadcastChannels: [""],
-
-  //auto DL
-  autoDownload: {
-    // Daftarkan semua ID SERVER tempat fitur ini boleh aktif
-    enabledServers: [
-      "", // Ganti dengan ID SERVER (GUILD ID) Anda
-      "",
+  broadcastChannels: [
+        ""
     ],
-    // (Opsional) Daftarkan ID CHANNEL yang ingin dikecualikan di server tersebut
-    excludedChannels: ["", ""],
-  },
+
+    //auto DL
+  autoDownload: {
+        // Daftarkan semua ID SERVER tempat fitur ini boleh aktif
+        enabledServers: [
+            "", // Ganti dengan ID SERVER (GUILD ID) Anda
+            ""
+        ],
+        // (Opsional) Daftarkan ID CHANNEL yang ingin dikecualikan di server tersebut
+        excludedChannels: [
+            "", 
+            ""
+        ]
+    },
 
   // API Keys
   apikey_lann: process.env.API_KEY_LANN, // Mengambil API key dari .env
-  // kamu bisa dapat apikey ini dengan beli di https://api.betabotz.eu.org
+// kamu bisa dapat apikey ini dengan beli di https://api.betabotz.eu.org
 
   apikey_dana: process.env.API_KEY_DANA, // Mengambil API key dari .env
-  // kamu bisa dapat apikey ini dengan beli di https://api.danafxc.my.id/Price-api
+// kamu bisa dapat apikey ini dengan beli di https://api.danafxc.my.id/Price-api
 
-  ownerId: "686498842560168043",
+
+  ownerId: '686498842560168043',
 
   // Menu & Feature Settings
   menuPublic: false, // Apakah menu public aktif?
@@ -59,6 +65,7 @@ module.exports = {
 };
 
 //isi dari .env
+
 
 //#DISCORD_BOT_TOKEN=""
 //API_KEY_LANN=""
